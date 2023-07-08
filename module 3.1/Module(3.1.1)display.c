29d43301182b06e25bb671721e6585662fd8e25e
Brijesh Vekariya <137178081+Brijeshvekariya@users.noreply.github.com>
2023-07-08 17:32:58 +0530

    Add files via upload

---
+++ module 3.1/Module(3.1.1)display.c
@@ -0,0 +1,18 @@
+/*
+	Q.1 Display This Information using printf :
+		1. Your Name
+		2. Your Birth date
+		3. Your Age
+		4. Your Address
+*/
+
+
+#include<stdio.h>
+
+int main()
+{
+	printf("My name is Brijesh Vekariya\n");
+	printf("my birth-date is 23 August 2000\n");
+	printf("My age is 22\n");
+	printf("My address is 134, Daimond Park society, Near Uttamnagar, Nikol road, Ahmedabad");
+}
